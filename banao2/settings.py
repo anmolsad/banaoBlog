4df8275a7ac5171d524935427471be2c9d7841f5
anmol sad <anmolsad02gmail.com>
2024-03-12 18:44:09 +0530

    deploy

--- banao2/settings.py
+++ banao2/settings.py
@@ -128,5 +128,3 @@ MEDIA_URL = '/media/'
   
 # Path where media is stored  
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')  
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
